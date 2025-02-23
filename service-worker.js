8cd25b85ec0cad098c952501b258e37406b18cbd
airbr <morgan@morganwebdev.com>
2025-02-22 19:12:40 -0800

    Offline #3

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,32 @@
+// Establish a cache name
+const cacheName = 'WSGCACHE';
 
+// Assets to precache
+const precachedAssets = [
+  '/index.html',
+  '/js/app.js',
+  '/css/styles.css'
+];
 
-const cacheName = "wsgcache";
-const precachedResources = ["/index.html", "/js/app.js", "/css/style.css"];
-
-async function precache() {
-  const cache = await caches.open(cacheName);
-  return cache.addAll(precachedResources);
-}
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(precache());
+self.addEventListener('install', (event) => {
+  // Precache assets on install
+  event.waitUntil(caches.open(cacheName).then((cache) => {
+    return cache.addAll(precachedAssets);
+  }));
 });
 
-async function cacheFirst(request) {
-  const cachedResponse = await caches.match(request);
-  if (cachedResponse) {
-    return cachedResponse;
-  }
-  try {
-    const networkResponse = await fetch(request);
-    if (networkResponse.ok) {
-      const cache = await caches.open("wsgcache");
-      cache.put(request, networkResponse.clone());
-    }
-    return networkResponse;
-  } catch (error) {
-    return Response.error();
-  }
-}
+self.addEventListener('fetch', (event) => {
+  // Is this one of our precached assets?
+  const url = new URL(event.request.url);
+  const isPrecachedRequest = precachedAssets.includes(url.pathname);
 
-self.addEventListener("fetch", (event) => {
-  if (precachedResources.includes(url.pathname)) {
-    event.respondWith(cacheFirst(event.request));
+  if (isPrecachedRequest) {
+    // Grab the precached asset from the cache
+    event.respondWith(caches.open(cacheName).then((cache) => {
+      return cache.match(event.request.url);
+    }));
+  } else {
+    // Go to the network
+    return;
   }
 });
-
